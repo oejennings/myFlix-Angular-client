7d9e1d4e74b65c6f820e4c6028748cb81be2f4ce
Olivia Jennings <oejennings@users.noreply.github.com>
2023-12-12 14:04:57 -0500

    fixed format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -6,18 +7,19 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component'
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        UserRegistrationFormComponent
+        UserRegistrationFormComponent,
+        UserLoginFormComponent
     ],
     imports: [
         BrowserModule,
