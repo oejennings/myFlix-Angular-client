9bd309aa6794928700c62af42ec25a891a8a516f
Olivia Jennings <oejennings@users.noreply.github.com>
2023-12-12 12:29:33 -0500

    fixed class name

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -11,7 +11,7 @@ const apiUrl = 'https://oj-movies-0c0784fe26f8.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
